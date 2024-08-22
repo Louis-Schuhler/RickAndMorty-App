94a799fcb08bdfd84fddce4645536638bbce32f3
Louis Schuhler 🤿 <Louis.Schuhlerpro@gmail.com>
2024-08-22 09:51:20 +0200

    Create RMCharacterViewController.swift

---
+++ RickAndMorty/Controllers/Core/RMCharacterViewController.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+// final means it cannot be subclassed
+
+/// Controllers to show and search characters
+final class RMCharacterViewController: UIViewController, RMCharacterListViewDelegate {
+    
+    private let characterListView = RMCharacterListView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        title = "Characters"
+        setupView()
+    }
+    
+    private func setupView() {
+        characterListView.delegate = self
+        view.addSubview(characterListView)
+        NSLayoutConstraint.activate([
+            characterListView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            characterListView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            characterListView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            characterListView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        
+    }
+    
+    // MARK: - RMCharacterListViewDelegate
+    
+    func rmCharacterListView(_ characterListView: RMCharacterListView, didSelectCharacter character: RMCharacter) {
+        // open detail controller for that character
+        let viewModel = RMCharacterDetailViewViewModel(character: character)
+        let detailVC = RMCharacterDetailViewController(viewModel: viewModel)
+        detailVC.navigationItem.largeTitleDisplayMode = .never 
+        navigationController?.pushViewController(detailVC, animated: true)
+    }
+    
+}
