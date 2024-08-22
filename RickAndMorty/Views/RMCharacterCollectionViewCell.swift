e5f6bb3a8e7becab64f6091e3ccab76eea82a995
Louis Schuhler 🤿 <Louis.Schuhlerpro@gmail.com>
2024-08-22 09:26:53 +0200

    Create RMCharacterCollectionViewCell.swift

---
+++ RickAndMorty/Views/RMCharacterCollectionViewCell.swift
@@ -0,0 +1,109 @@
+import UIKit
+
+/// Template of a single Cell.
+final class RMCharacterCollectionViewCell: UICollectionViewCell {
+    static let cellIdentifier = "RMCharacterCollectionViewCell"
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true;
+        imageView.translatesAutoresizingMaskIntoConstraints = false;
+        return imageView
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.font = .systemFont(ofSize: 18, weight: .medium)
+        label.translatesAutoresizingMaskIntoConstraints = false;
+        return label
+    }()
+    
+    private let statusLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .secondaryLabel
+        label.font = .systemFont(ofSize: 16, weight: .regular)
+        label.translatesAutoresizingMaskIntoConstraints = false;
+        return label
+    }()
+    
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        contentView.backgroundColor = .secondarySystemBackground
+        // content view is a function from cell that takes care of adding the subviews but with proper constraints.
+        contentView.addSubviews(imageView, nameLabel, statusLabel)
+        addConstraints();
+        setUpLayer();
+    }
+    
+    // If you are not using storyboards, you will need to tell Xcode this.
+    required init?(coder: NSCoder){
+        fatalError("Unsupported")
+    }
+    
+    private func setUpLayer(){
+        contentView.layer.cornerRadius = 8
+        contentView.layer.shadowColor = UIColor.secondaryLabel.cgColor
+        contentView.layer.cornerRadius = 4
+        contentView.layer.shadowOffset = CGSize(width: -4, height: 4)
+        contentView.layer.shadowOpacity = 0.4
+    }
+    
+    private func addConstraints(){
+        NSLayoutConstraint.activate([
+            statusLabel.heightAnchor.constraint(equalToConstant: 30),
+            nameLabel.heightAnchor.constraint(equalToConstant: 30),
+            
+            statusLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 7),
+            statusLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -7),
+            nameLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 7),
+            nameLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -7),
+            
+            statusLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -3),
+            nameLabel.bottomAnchor.constraint(equalTo: statusLabel.topAnchor),
+            
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.leftAnchor.constraint(equalTo: contentView.leftAnchor),
+            imageView.rightAnchor.constraint(equalTo: contentView.rightAnchor),
+            imageView.bottomAnchor.constraint(equalTo: nameLabel.topAnchor, constant: -3)
+        ])
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        setUpLayer()
+    }
+    
+    // every time its reused we want to reset the cell
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        imageView.image = nil;
+        nameLabel.text = nil;
+        statusLabel.text = nil
+    }
+    
+    // custom function that will take care of populating the cell with the right data thanks to the Model.
+    public func configure(with viewModel: RMCharacterCollectionViewCellViewModel){
+        nameLabel.text = viewModel.characterName
+        statusLabel.text = viewModel.characterStatusText
+        viewModel.fetchImage { [weak self] result in // pointer (memory leaks and retain cycles topic)
+            switch result{
+            case .success(let data):
+                DispatchQueue.main.async {
+                    let image = UIImage(data: data)
+                    self?.imageView.image = image
+                }
+            case .failure(let error):
+                print(error)
+                break
+            }
+        }
+    }
+    
+    
+    
+}
