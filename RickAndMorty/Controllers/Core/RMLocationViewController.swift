5204d72d60309fa4936ec55deba335fe7e83f815
Louis Schuhler 🤿 <Louis.Schuhlerpro@gmail.com>
2024-08-22 09:50:23 +0200

    Create RMLocationViewController.swift

---
+++ RickAndMorty/Controllers/Core/RMLocationViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+// final means it cannot be subclassed
+
+/// Controllers to show and search locations
+final class RMLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground;
+        title = "Locations"
+    }
+    
+
+}
