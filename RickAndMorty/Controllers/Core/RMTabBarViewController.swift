52523f69688da7ee9726f9f69509d772b627f3c7
Louis Schuhler 🤿 <Louis.Schuhlerpro@gmail.com>
2024-08-22 09:49:26 +0200

    Create RMTabBarViewController.swift

---
+++ RickAndMorty/Controllers/Core/RMTabBarViewController.swift
@@ -0,0 +1,50 @@
+import UIKit
+
+// final means it cannot be subclassed
+
+/// Controller to house tabs and root tab controllers
+final class RMTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red;
+        setUpTabs()
+    }
+    
+    private func setUpTabs(){
+        // instatiating all controllers
+        let charactersVC = RMCharacterViewController()
+        let locationsVC = RMLocationViewController()
+        let episodesVC = RMEpisodeViewController()
+        let settingsVC = RMSettingsViewController()
+        
+        charactersVC.navigationItem.largeTitleDisplayMode = .automatic
+        locationsVC.navigationItem.largeTitleDisplayMode = .automatic
+        episodesVC.navigationItem.largeTitleDisplayMode = .automatic
+        settingsVC.navigationItem.largeTitleDisplayMode = .automatic
+        
+        // creating top bar navigations for each controller
+        let nav1 = UINavigationController(rootViewController: charactersVC)
+        let nav2 = UINavigationController(rootViewController: locationsVC)
+        let nav3 = UINavigationController(rootViewController: episodesVC)
+        let nav4 = UINavigationController(rootViewController: settingsVC)
+        
+        // set tab bar item
+        nav1.tabBarItem = UITabBarItem(title: "Characters", image: UIImage(systemName: "person"), tag: 1)
+        nav2.tabBarItem = UITabBarItem(title: "Locations", image: UIImage(systemName: "globe"), tag: 2)
+        nav3.tabBarItem = UITabBarItem(title: "Episodes", image: UIImage(systemName: "tv"), tag: 3)
+        nav4.tabBarItem = UITabBarItem(title: "Settings", image: UIImage(systemName: "gear"), tag: 4)
+        
+        for nav in [nav1, nav2, nav3, nav4] {
+            nav.navigationBar.prefersLargeTitles = true;
+            
+        }
+        
+        // FUNCTION available from the UITabBar controller we are inheriting from
+        setViewControllers(
+            [nav1, nav2, nav3, nav4],
+            animated: true)
+    }
+
+
+}
