2bfd5a4dfcc90a770e1a6c7664b90efd64528979
Louis Schuhler 🤿 <Louis.Schuhlerpro@gmail.com>
2024-08-22 09:49:52 +0200

    Create RMSettingsViewController.swift

---
+++ RickAndMorty/Controllers/Core/RMSettingsViewController.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+// final means it cannot be subclassed
+
+/// Controller to show various app and options settings
+final class RMSettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground;
+        title = "Settings"
+    }
+    
+
+
+}
