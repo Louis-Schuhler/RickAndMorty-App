d7793ae3109a56b172aa20a1617de5559ec95ae9
Louis Schuhler 🤿 <Louis.Schuhlerpro@gmail.com>
2024-08-22 09:43:19 +0200

    Create RMCharacter.swift

---
+++ RickAndMorty/Models/DataTypes/RMCharacter.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+struct RMCharacter: Codable { // Codable lets you decode and deserialize JSON
+    let id: Int
+    let name: String
+    let status: RMCharacterStatus
+    let species: String
+    let type: String
+    let gender: RMCharacterGender
+    let origin: RMOrigin
+    let location: RMSingleLocation
+    let image: String
+    let episode: [String]
+    let url: String
+    let created: String
+}
