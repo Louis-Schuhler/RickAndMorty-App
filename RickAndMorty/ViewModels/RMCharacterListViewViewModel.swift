5c2ee1b1d5e8fe91366b99850324d31bcd21935f
Louis Schuhler 🤿 <Louis.Schuhlerpro@gmail.com>
2024-08-22 09:28:11 +0200

    Create RMCharacterListViewViewModel.swift

---
+++ RickAndMorty/ViewModels/RMCharacterListViewViewModel.swift
@@ -0,0 +1,141 @@
+import Foundation
+import UIKit
+
+protocol RMCharacterListViewViewModelDelegate: AnyObject{
+    func didLoadInitialCharacters()
+    func didSelectCharacter(_ character: RMCharacter)
+}
+
+/// View Model to handle character list view logic
+final class RMCharacterListViewViewModel: NSObject {
+    
+    public weak var delegate: RMCharacterListViewViewModelDelegate?
+    
+    private var isLoadingMoreCharacters = false
+    
+    private var characters: [RMCharacter] = []{
+        didSet{
+            for character in characters {
+                let viewModel = RMCharacterCollectionViewCellViewModel(
+                    characterName: character.name,
+                    characterStatus: character.status,
+                    characterImageUrl: URL(string: character.image)
+                )
+                cellViewModels.append(viewModel) // append the view models to the Array
+            }
+        }
+    }
+    
+    // view models array, hold on to these view models.
+    private var cellViewModels: [RMCharacterCollectionViewCellViewModel] = []
+    
+    private var apiInfo: RMGetAllCharactersResponse.Info? = nil;
+    
+    /// Fetch initial set of characters (20)
+    func fetchCharacters(){
+        RMService.shared.execute(
+            .listCharactersRequests,
+            expecting: RMGetAllCharactersResponse.self
+        ) {[weak self] result in
+            switch result {
+            case .success(let responseModel):
+                let results = responseModel.results
+                let info = responseModel.info
+                self?.characters = results
+                self?.apiInfo = info
+                DispatchQueue.main.async {
+                    self?.delegate?.didLoadInitialCharacters()
+                }
+            case .failure(let error):
+                print(String(describing: error))
+            }
+        }
+    }
+    
+    /// Pagnate if additional characters are needed
+    public func fetchAdditionalCharacters(){
+        // Fetch characters here
+        
+    }
+    
+    public var shouldShowLoadMoreIndicator: Bool {
+        return apiInfo?.next != nil;
+    }
+    
+}
+
+// MARK: - CollectionView
+
+extension RMCharacterListViewViewModel: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cellViewModels.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // pass the right cell for the view model.
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: RMCharacterCollectionViewCell.cellIdentifier,
+            for: indexPath
+        ) as? RMCharacterCollectionViewCell else {
+            fatalError("Unsupported cell")
+        } //  cast as a cell we want.
+        
+        let viewModel = cellViewModels[indexPath.row]
+        
+        cell.configure(with: viewModel)
+        
+        return cell;
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        guard kind == UICollectionView.elementKindSectionFooter, let footer = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: RMFooterLoadingCollectionReusableView.identifier, for: indexPath) as? RMFooterLoadingCollectionReusableView else {
+            fatalError("Unsupported")
+        }
+        
+        footer.startAnimating()
+        return footer
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        
+        guard shouldShowLoadMoreIndicator else {
+            return .zero
+        }
+        
+        return CGSize(width: collectionView.frame.width, height: 100)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let bounds = UIScreen.main.bounds
+        // calculating gaps between cells
+        let width = (bounds.width-30)/2
+        return CGSize(width: width, height: width*1.5)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        let character = characters[indexPath.row] // get the character we tapped on
+        delegate?.didSelectCharacter(character)
+    }
+}
+
+// MARK: ScrollView
+extension RMCharacterListViewViewModel: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        //
+        guard shouldShowLoadMoreIndicator, !isLoadingMoreCharacters else { // not currently loading more characters
+            return
+        }
+        
+        let offset = scrollView.contentOffset.y
+        let totalContentHeight = scrollView.contentSize.height
+        let totalScrollViewFixedHeight = scrollView.frame.size.height
+        
+        if offset >= (totalContentHeight - totalScrollViewFixedHeight - 120){ // 120 is a nice little buffer
+            print("Should start fetching more")
+            isLoadingMoreCharacters = true;
+        }
+        
+    }
+}
